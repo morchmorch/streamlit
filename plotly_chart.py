ed37f2b9df3882e47bea6363d86bb2f38c5d3213
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-03 21:49:54 -0400

    fifth commit

--- plotly_chart.py
+++ plotly_chart.py
@@ -101,25 +101,51 @@ def clean_data () :
     return df_sources_custom
 
 
-def draw_f_fig (df_custom, sector_optionn) :
+def draw_f_fig (df_custom, sector_option) :
+
+    if sector_option is not 'All':
+        df_custom = df_custom [ df_custom.Sector == sector_option  ]. sort_values(by =  'Net Income Margin % (FY)')
+    else :
+        df_custom = df_custom. sort_values(by =  'Net Income Margin % (FY)')
+
+    camera = dict ( up=dict(x=1.5, y=0, z=1), center=dict(x=0, y=0, z=0), eye=dict(x=2.5, y=1.5, z=2) )
 
     fig = px.scatter_3d(
-            df_custom [ df_custom.Sector == sector_option ],
+            df_custom ,
             #x="Profit Margin",
             #y="Industry",
-            z = 'Profit Margin',
-            y = 'Sales growth quarter over quarter' ,
+            z = 'Total Revenues/CAGR (2Y FY)',
+            y =  'Net Income Margin % (FY)' ,
             x='Industry',
             width=1000,
             height=800,
-            #hover_name="Company",
-            hover_data= ['Company','Market Cap','Profit Margin'],
+            hover_name="Name",
+            #hover_data= ['Company','Market Cap','Profit Margin'],
+            hover_data= ['Name', 'Ticker', 'Industry',  'Total Revenues/CAGR (2Y FY)', 'Net Income Margin % (FY)'],
             #size = 'Market Cap',
+            labels={ 'Total Revenues/CAGR (2Y FY)' : 'Revenues CAGR (2YFY)' ,'Net Income Margin % (FY)':'NI Margin(%)', "Industry": ""} ,
             color = 'Industry',
             color_continuous_scale=px.colors.sequential.RdBu_r,
             #template="plotly_white"
 
 
+    )
+    fig.update_layout(
+        scene = dict(
+            xaxis = dict (nticks=0,ticktext =["x"], ticks='outside', gridcolor="white", showbackground=True,backgroundcolor="rgb(200, 200, 230)",
+            tickfont=dict(
+                                color='white',
+                                size=1,
+                                family='Old Standard TT, serif',)
+            ) ,
+            xaxis_title='',
+            #xaxis_showspikes=False,
+            yaxis = dict(nticks=0, backgroundcolor="rgb(230, 230,200)" ),
+            zaxis = dict( nticks=0 ,ticktext =[""] ),
+            
+            
+            camera=camera
+        ),
     )
     #st.sidebar.multiselect( "Please select the sector:", options=df_custom["Sector"].unique(),)
 
@@ -142,7 +168,7 @@ def draw_t_fig (df_custom, sector_optionn) :
             width=1000,
             height=800,
             #hover_name="Company",
-            hover_data= ['Company','Market Cap','Profit Margin'],
+            hover_data= ['Name', 'Ticker' ],
             #size = 'Market Cap',
             color = 'Industry',
             color_continuous_scale=px.colors.sequential.RdBu_r,
@@ -161,15 +187,33 @@ def draw_t_fig (df_custom, sector_optionn) :
 
 #df_custom = clean_data()
 
-df_custom = pd.read_csv ( "https://investrecipes.s3.amazonaws.com/apps/stockcharts_as/industries-rsi-adx-consolidated-stockcharts.csv")
+tdf = pd.read_csv ( "https://investrecipes.s3.amazonaws.com/apps/stockcharts_as/industries-rsi-adx-consolidated-stockcharts.csv")
+
+kdf = pd.read_csv ('https://investrecipes.s3.amazonaws.com/koyfin_all_stocks.csv')
+
+kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (2Y FY)'] / kdf[ 'EV/Sales (EST FY1)' ]
+
+kdf['growth_evsales_ratio'] = pd.to_numeric (kdf['growth_evsales_ratio'])
+
+kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] > 10 ]
+
+kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] < 100 ]
+
+kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] > 10 ]
+
+kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] < 1000 ]
 
 
 
+df_custom = fdf.copy()
+l = df_custom.Sector.unique().tolist()
+l.append('All')
+sector_option = st.radio( "Sector",  l  )
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
-sector_option =  st.selectbox ( 'Select Sector', df_custom.Sector.unique().tolist() )
+#sector_option =  st.selectbox ( 'Select Sector', df_custom.Sector.unique().tolist() )
 
 
-draw_f_fig(df_custom, sector_option)
 draw_t_fig(df_custom, sector_option)
 
 #st.plotly_chart(fig)
