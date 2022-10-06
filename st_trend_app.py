b03e3a0eaa15784058f7a93b94fbc8370c8a6d60
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-06 11:37:47 -0400

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -124,7 +124,7 @@ kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] < 1000 ]
 #st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 #sector_option =  st.selectbox ( 'Select Sector', df_custom.Sector.unique().tolist() )
-
+st.set_page_config(page_title="Investrecipes",layout='wide')
 
 draw_trend_fig()
 draw_external_fig()
