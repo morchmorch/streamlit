3e76ac83087630f8ff5569acd01b794b1215d403
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-10 13:33:15 -0400

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -103,9 +103,9 @@ def draw_external_fig():
     st.caption ( ', '.join (df[cols].symbols.tolist()) )
     st.write(df[cols])
     
-    st.write (  take_string_give_url ( sector_option ).split('-agg')[0] + '-charts.png' )
+    #st.write (  take_string_give_url ( sector_option ).split('-agg')[0] + '-charts.png' )
  
-    st.image (  take_string_give_url ( sector_option ).split('-agg')[0] + '-charts.png' )
+    st.image (  take_string_give_url ( sector_option ).split('.csv')[0] + '-charts.png' )
 
 def draw_technical_fig():
 
