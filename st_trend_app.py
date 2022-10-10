b6ba47f86d9a8bd19d85367169f64cb8354d9981
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-10 16:25:57 -0400

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -101,11 +101,14 @@ def draw_milestone_fig():
     html_page =  take_string_give_url ( sector_option ).split('-agg')[0] + ".html"
     df = pd.read_html ( html_page )[0]
     st.caption ('fundamentals')
-    try :
-        st.write(df[cols])
-    except :
-        st.write (df ['Symbol','Name','SCTR'] )
- 
+
+    if not df.empty :
+
+        try :
+            st.write(df[cols])
+        except :
+            st.write (df ['Symbol','Name','SCTR'] )
+     
 def draw_external_fig():
 
 
