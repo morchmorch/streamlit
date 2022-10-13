4734e268af64d1ccd9654e13f9fb3090d463cf46
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-13 16:19:20 -0400

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -228,8 +228,7 @@ def draw_momentum_figs():
     adf = pd.read_html('https://investrecipes.s3.amazonaws.com/all-files.html')[0]
 
     if 'industries' in sector_option :
-        images = [ x for x in adf.key.tolist() if x.startswith ('industry_') and x.endswith('.png') and 'rrg' in x  and 'industries_' in x ]
-        images = [x for x in adf.key.tolist() if 'industries_rrg' in x and 'stockworld' in x]
+        images = [ x for x in adf.key.tolist() if 'rrg' in x and 'industries_' in x]
     if 'companies' in sector_option:
         images = [ x for x in adf.key.tolist() if x.startswith ('industry_') and x.endswith('.png') and 'rrg' in x  and 'companies' in x ]
 
