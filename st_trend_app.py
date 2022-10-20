f7fdb6f2d5e94dea42f4659ee5c3821ef0184189
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-20 19:46:55 -0400

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -400,7 +400,7 @@ kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] < 1000 ]
 st.set_page_config(page_title="Investrecipes",layout='wide')
 
 
-tab1, tab2,tab3,tab4,tab5,tab6 = st.tabs([" (stocks-technical analysis) ", " (etfs - technical analysis) ", " (market - weekly performance) " , " (momentum views) ", " (fundamental explore) ", " ( technical explore ) " ])
+tab1, tab2,tab3,tab4,tab5,tab6 = st.tabs([ " (etfs - technical analysis) ", "(stocks-technical analysis)", " (market - weekly performance) " , " (momentum views) ", " (fundamental explore) ", " ( technical explore ) " ])
 
 
 with tab1:
