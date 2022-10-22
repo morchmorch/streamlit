b087dc29d085d08a4018dcdcb0fef4216e9f0cd4
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-21 20:15:18 -0400

    fifth commit

--- st_job_trend.py
+++ st_job_trend.py
@@ -254,10 +254,11 @@ print (must_term)
 
 joindf=joindf[joindf.Job_Title.str.lower().str.contains (must_term) ]
 
-joindfd=joindf[joindf.Job_Title.str.lower().str.contains('|'.join(tlist) , na=False)
+joindfd=joindf[joindf.Job_Title.str.lower().str.contains('|'.join(tlist) , na=False) ]
 
 
 l = joindfd.Company_Name.tolist()
+
 sector_option = st.radio( "Hiring Companies",  l  )
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
