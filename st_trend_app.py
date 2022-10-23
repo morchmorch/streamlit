6fd2e2582b0f65d3f3e60f1fd973371dfb32d846
Venkat Pothamsetty <rkreddy@gmail.com>
2022-10-23 14:33:59 -0400

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -460,7 +460,7 @@ with tab3:
 
 with tab4:
     st.header("(momentum views across stocks, industries, etfs )")
-    l = ['silver cross', 'gollden cross']
+    l = ['silver cross', 'golden cross']
     sector_option = st.radio( "Relative Rotation Graphs, Industries and Companies in each Sector",  l  )
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
     col1, col2 , col3 = st.columns(3)
