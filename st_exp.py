0f69df70060b8f20afce0a790c2330fe26f52c57
Venkat Pothamsetty <rkreddy@gmail.com>
2022-11-09 13:45:31 -0500

    fifth commit

--- st_exp.py
+++ st_exp.py
@@ -481,6 +481,7 @@ st.write(l)
 sector_option = st.radio( "Hiring Companies",  l )
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
+st.write ('https://investrecipes.s3.amazonaws.com/' + sector_option)
 
 
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
