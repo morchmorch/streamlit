6e7729611ed66720ac7f16741c9d06f9b9d369ad
Venkat Pothamsetty <rkreddy@gmail.com>
2022-11-09 04:54:18 -0500

    fifth commit

--- st_exp.py
+++ st_exp.py
@@ -427,3 +427,14 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write("""
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+
