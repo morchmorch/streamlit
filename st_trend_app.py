a84f90bea28bdb34cf216c7b422b1162521c2e6b
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-05 09:44:22 -0500

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -274,7 +274,7 @@ def draw_technical_fig():
     st.caption ('charts')
     st.image (  take_string_give_url ( sector_option ).split('.csv')[0] + '.png' )
     #st.write ( take_string_give_url ( sector_option ).split('-agg')[0] + '-rrg.png' )
-    st.image (  take_string_give_url ( sector_option ).split('-agg')[0] + '-rrg.png' )
+    #st.image (  take_string_give_url ( sector_option ).split('-agg')[0] + '-rrg.png' )
     st.markdown("""---""") 
 
     cols = ['Ticker','Company','Profit Margin','Sales growth quarter over quarter']
