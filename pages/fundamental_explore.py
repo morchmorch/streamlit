c124d42287b9d82c33c38780e0ddfd49081cb2c0
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-07 11:57:52 -0500

    fifth commit

--- pages/fundamental_explore.py
+++ pages/fundamental_explore.py
@@ -104,6 +104,7 @@ def draw_f_fig () :
     #kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] > 10 ]
 
     kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] < 100 ]
+    kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] >0 ]
 
     #kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] > 10 ]
 
