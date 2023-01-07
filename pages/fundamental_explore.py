fd4b7752d6ff62160a6daa778575d33cdca61efc
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-07 11:23:43 -0500

    fifth commit

--- pages/fundamental_explore.py
+++ pages/fundamental_explore.py
@@ -96,7 +96,7 @@ def draw_f_fig () :
 
     kdf = pd.read_csv ('https://investrecipes.s3.amazonaws.com/koyfin_all_companies.csv')
 
-    kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (1Y FY)'] / kdf[ 'EV/Sales (EST FY1)' ]
+    kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (2Y FY)'] / kdf[ 'EV/Sales (EST FY1)' ]
 
     kdf['growth_evsales_ratio'] = pd.to_numeric (kdf['growth_evsales_ratio'])
 
