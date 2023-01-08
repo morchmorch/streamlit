8b206ee7b87eb008587a950de527d9021e8a4d43
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-08 08:54:33 -0500

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -97,7 +97,7 @@ def draw_f_fig () :
     kdf = pd.read_csv ('https://investrecipes.s3.amazonaws.com/koyfin_all_companies.csv')
 
     st.write (kdf.columns.tolist())
-    kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (1Y FY)'] / kdf[ 'EV/Sales (EST FY1)' ]
+    kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (1Y FY)'] / kdf[ 'EV/Sales (NTM)' ]
 
     kdf['growth_evsales_ratio'] = pd.to_numeric (kdf['growth_evsales_ratio'])
 
