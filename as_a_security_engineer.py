0344f708dc95be67fd6f484783f3720b4aac55fe
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-17 11:31:05 -0500

    fifth commit

--- as_a_security_engineer.py
+++ as_a_security_engineer.py
@@ -46,7 +46,7 @@ st.title ('As a Security Engineer, I want to ... ')
 
 button_name = "Write it for me !! "
 response_while = "Right on it, it will be only couple of seconds ..."
-response_after = "Here you go ... : "
+response_after = "Here you go ...  "
 
 standards_tab, iam_tab, monitor_tab, bp_tab, raf_tab, rs_tab = st.tabs([  "Author a standards policy", "Author a secure IAM policy", "Detect Vulnerabilities, Monitor Logs and Alerts", "Recommend a Best Practice", "Reduce Attack Surface", " Keep an eye on Spend (follow $$$)"] )
 
