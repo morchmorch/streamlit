5c50a9f58f56bb4d4030df1cc346a79f02453517
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-17 12:02:14 -0500

    fifth commit

--- as_a_security_engineer.py
+++ as_a_security_engineer.py
@@ -41,31 +41,6 @@ def get_write_response (base_prompt) :
 ## main
 
 st.set_page_config(page_title="Security Engineer Companion",layout='wide')
-
-st.markdown(""" 
-div.stButton > button:first-child {
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
-}
-.css-2trqyj:focus:not(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:active){
-background-color: #0066cc;
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-} """)
-
 st.header ('As a Cloud Security Engineer, I want to ... ')
 
 button_name = "Write it for me !! "
