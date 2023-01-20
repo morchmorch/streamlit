9d06fafa74b7f6e4ce1512c9829947fcfc379c22
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-19 20:28:08 -0500

    fifth commit

--- as_a_security_engineer.py
+++ as_a_security_engineer.py
@@ -314,7 +314,7 @@ with rs_tab :
         base_prompt = """ python script to to find current aws spend per day for the last 10 days, group by service , give the results in a pandas dataframe . use json_normalize with errors='ignore' .  the start time argument to get_cost_and_usage format should be yyyy-MM-dd.  convert keys column in the dataframe to string before groupby """ 
     
     if "spend grouped by account" in s_type.lower () :
-        base_prompt = """ write a python script to find current aws spend per day for the last 10 days, pass groupby dimension of LINKED_ACCOUNT into get_cost_and_usage() api call, give the results in a pandas dataframe . comment the code and remove all empty lines from the function. the start time argument to get_cost_and_usage format should be yyyy-MM-dd with start date 10 days back and end date of yesterday.  do not use python functions """
+        base_prompt = """ write a python script to find current aws spend per day for the last 10 days, pass groupby the Type DIMENSION and Key  LINKED_ACCOUNT into get_cost_and_usage() api call, give the results in a pandas dataframe . comment the code and remove all empty lines from the function. the start time argument to get_cost_and_usage format should be yyyy-MM-dd with start date 10 days back and end date of yesterday.  do not use python functions """
 
     #question=st.text_area("Input the Question Here")
     rs_button=st.button(button_name, key = "rs_tab_button")
