01901d8b9ba898f1e3b0d4da043714479a0b7b26
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-23 23:04:37 -0500

    fifth commit

--- as_a_professor.py
+++ as_a_professor.py
@@ -131,7 +131,8 @@ for tab in tabs :
         df_d = df [ df.tasks == tab_name ]
         dropdowns = df [ df.tasks == tab_name ].dropdown
         st.write (dropdowns)
-        service=st.selectbox("Select the service:", dropdowns, key='dropdowns-'+ str (randrange(10000) ) )
+        #service=st.selectbox("Select the service:", dropdowns, key='dropdowns-'+ str (randrange(10000) ) )
+        service=st.selectbox("Select the service:", dropdowns )
         st.write (service)
         prompt = df_d [df_d.dropdown == service].prompt.tolist()[0]
         st.write(prompt)
