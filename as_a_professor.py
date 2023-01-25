4c0e40462b25306223db818749a8341428680ed5
Venkat Pothamsetty <rkreddy@gmail.com>
2023-01-25 17:25:53 -0500

    fifth commit

--- as_a_professor.py
+++ as_a_professor.py
@@ -61,7 +61,7 @@ def draw_prompt(dropdowns, tabname, df_d):
         select = df_d1.dropdownname.unique().tolist()[0]
         s_d = st.radio ( str (select) + " : ", dropdowns , key = "dropdowns" + str( tabname) )
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-        tab_button=st.button(button_name , key = tab_name)
+        tab_button=st.button(button_name , key = tab_name + "1")
         base_prompt = df_d [df_d.dropdown == s_d].prompt.unique().tolist()[0]
         st.markdown ( "--------")
         if tab_button:
@@ -71,7 +71,7 @@ def draw_prompt(dropdowns, tabname, df_d):
         select = df_d2.dropdownname.unique().tolist()[0]
         s_d = st.radio ( str (select) + " : ", dropdowns , key = "dropdowns" + str( tabname) )
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-        tab_button=st.button(button_name , key = tab_name)
+        tab_button=st.button(button_name , key = tab_name+"2")
         base_prompt = df_d [df_d.dropdown == s_d].prompt.unique().tolist()[0]
         st.markdown ( "--------")
         if tab_button:
