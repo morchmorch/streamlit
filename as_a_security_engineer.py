49c985771aac2af0ecc0087316ef5b6f987897d8
Venkat Pothamsetty <rkreddy@gmail.com>
2023-03-04 08:42:57 -0500

    fifth commit

--- as_a_security_engineer.py
+++ as_a_security_engineer.py
@@ -27,8 +27,8 @@ def get_write_response (base_prompt) :
     with st.spinner ( response_while ) :
         answer=response1(base_prompt)
         st.subheader (response_after)
-        st.code(answer, language="python")
-
+        #st.code(answer, language="python")
+        st.markdown(answer)
 
 
 ## main
