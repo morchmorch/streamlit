c5270d8ef8f776a33437cebcf0b537c48e2b52f8
Venkat Pothamsetty <rkreddy@gmail.com>
2023-03-08 08:26:23 -0500

    fifth commit

--- as_a_sales_manager.py
+++ as_a_sales_manager.py
@@ -64,69 +64,6 @@ def draw_prompt2(dropdowns, tabname, df_d):
 
 
 
-## main
-
-#st.set_page_config(page_title="Draft it for Me - Sales ",layout='wide', page_icon=None)
-
-#m = st.markdown("""
-#<style>
-#div.stButton > button:first-child {
-#    background-color: #0099ff;
-#    color:#ffffff;
-#}
-#div.stButton > button:hover {
-#    background-color: #00fffg;
-#    color:#ffffff;
-#    }
-#</style>""", unsafe_allow_html=True)
-
-
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: #0099ff;
-    color:#ffffff;
-}
-</style>""", unsafe_allow_html=True)
-
-
-footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-color: blue;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p> feedback, feature requests, bugs to <a href="mailto:rkreddy@gmail.com">rkreddy@gmail.com</a>   --  AI assisted drafts for other roles, visit our <a href="https://www.draftitforme.com">homepage</a> </p>
-</div>
-"""
-
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-st.markdown(footer,unsafe_allow_html=True)
-
 
 button_name = "Draft it for me !! "
 response_while = "Right on it, it should be around 2-5 seconds ..."
