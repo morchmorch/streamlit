65f1f157f701b2710f56a35ad932e69a1dc9037e
Venkat Pothamsetty <rkreddy@gmail.com>
2023-03-17 13:32:57 -0400

    fifth commit

--- detect_bec_compromise.py
+++ detect_bec_compromise.py
@@ -94,7 +94,7 @@ def display_text () :
         jsonres = json.loads(res.split('Verdict:')[0])  
         cols = [ "urgency", "lack of detail", "attachments", "generic salutation",   "unusual requests", "spelling and grammar"  ]
         df = pd.DataFrame( list(jsonres.items()) , columns=['Phishing Characterstic', 'Probability'])
-        df = df [ ( ~ df['Phishing Characterstic'].str.contains ("verdict")) &  ( ~ df['Phishing Characterstic'].str.contains ("attack")) ]
+        df = df [ ( ~ df['Phishing Characterstic'].str.contains ("verdict")) ||  ( ~ df['Phishing Characterstic'].str.contains ("attack")) ]
         st.dataframe(df)        
         fig = px.bar(df, x='Phishing Characterstic', y='Probability', color='Probability', color_continuous_scale=px.colors.sequential.Plasma,
                      labels={'Probability':'Probability of Phishing'}, height=400)
