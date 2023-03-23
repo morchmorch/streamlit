229d7efd14ac7a1f398dde432b89ce661e4523b4
Venkat Pothamsetty <rkreddy@gmail.com>
2023-03-23 08:27:36 -0400

    fifth commit

--- as_a_k_kid.py
+++ as_a_k_kid.py
@@ -36,7 +36,7 @@ def streamlit_main (url) :
     df['tasks']= df['Subject']
     df['dropdown']= df['Topic'] + ":" + df ['Sub-Topic']
     df['dropdownname']= 'Select the course:'
-    df['prompt'] = 'teach me about ' + df_d ['dropdown'].unique().tolist()[0]    
+    df['prompt'] = 'teach me about ' + df ['dropdown'].unique().tolist()[0]    
 
     # tabs are the tasks
     tab_list = df.tasks.unique().tolist()
