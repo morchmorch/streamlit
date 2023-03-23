763906a505d3169427117a353801729313e15fab
Venkat Pothamsetty <rkreddy@gmail.com>
2023-03-23 15:45:23 -0400

    fifth commit

--- as_a_k_kid.py
+++ as_a_k_kid.py
@@ -58,7 +58,7 @@ def streamlit_main (url) :
         df['Topic'] = df['Topics']
     if 'Sub-Topic' not in df.columns.tolist():
         df['Sub-Topic'] = df['Sub-Topics']
-     df['tasks']= df['Subject']
+    df['tasks']= df['Subject']
     df['dropdown']= df['Topic'] + ":" + df ['Sub-Topic']
     df['dropdownname']= 'Select the course:'
     df['prompt'] = 'for ' + grade + ' grade teach me about ' + df['Sub-Topic'] + " in the topic of " + df['Topic']
