02e831d17ac4a2e3109ce6295fd033e21ffa6b09
Venkat Pothamsetty <rkreddy@gmail.com>
2023-03-23 08:21:24 -0400

    fifth commit

--- as_a_k_kid.py
+++ as_a_k_kid.py
@@ -34,7 +34,7 @@ def streamlit_main (url) :
     st.header ( role.strip() )
 
     df['tasks']= df['Subject']
-    df['dropdown']= df['Topic']
+    df['dropdown']= df['Topic'] + ":" + df ['Sub-Topic]
     df['dropdownname']= 'Select the course:'
     
 
