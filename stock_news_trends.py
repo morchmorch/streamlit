330974b3d78764bb9e7e6b685af64931ec8dcf6d
rkreddyp <rkreddy@gmail.com>
2023-05-24 13:35:00 +0000

    fifth commit

--- stock_news_trends.py
+++ stock_news_trends.py
@@ -19,23 +19,7 @@ st.title( 'Teach and Test K - 12 Grades')
 def streamlit_main (url) :
 
 
-    # Using object notation
-    st.sidebar.title("Select The Students Grade")
-    add_selectbox = st.sidebar.selectbox(
-        " ",
-        ("Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelveth")
-    )
-
-    #st.write (add_selectbox)
-
-    # Using "with" notation
-    #with st.sidebar:
-        #add_radio = st.radio(
-            #"Choose a shipping method",
-            #("Standard (5-15 days)", "Express (2-5 days)")
-        #)
-
-    #st.write(add_radio)
+    
 
     button_name = "Draft it for me !! "
     response_while = "Right on it, it should be around 2-5 seconds ..."
@@ -54,37 +38,6 @@ def streamlit_main (url) :
     
    
     
-    #grade = url.split(".csv")[0].split("/")[-1].lower()
-    url = "https://worldopen.s3.amazonaws.com/"+add_selectbox.lower()+".csv"
-    grade = add_selectbox.lower()
-    #url = 'https://worldopen.s3.amazonaws.com/prompts_sales.csv'
-    r = requests.get(url, allow_redirects=True)
-
-    open('/tmp/df.csv', 'wb').write(r.content)
-
-    df = pd.read_csv ('/tmp/df.csv', encoding = 'cp1252')
-    #st.dataframe(df)
-    #role = df.job.unique().tolist()[0]
-    role = 'Teach and Test K - 12 Grades'
-    #st.header ( role.strip() )
-    cols = [x for x in df.columns.tolist() if 'Unnamed' not in x]
-    df = df [cols]
-    df.columns = ['Subject', 'Topic', 'Sub-Topic']
-    #df ['Subject'] = df ['subject']
-    #df ['Topic'] = df ['topic']
-    #df ['Sub-Topic'] = df [ 'sub-topic']
-    #if 'Subject' not in df.columns.tolist():
-        #df['Subject'] = df['Subjects']
-    #if 'Topic' not in df.columns.tolist():
-        #df['Topic'] = df['Topics']
-    #if 'Sub-Topic' not in df.columns.tolist():
-        #df['Sub-Topic'] = df['Sub-topics']
-    df['tasks']= df['Subject']
-    df['dropdown']= df['Topic'] + ":" + df ['Sub-Topic']
-    df['dropdownname']= 'Select the course:'
-    df['prompt'] = 'for ' + grade + ' grade teach me about ' + df['Sub-Topic'] + " in the topic of " + df['Topic']
-    df['testprompt'] = 'for ' + grade + ' grade test me about ' + df['Sub-Topic'] + " in the topic of " + df['Topic']
-
     industries = ['biotechnology', 'pharmaceuticals', 'medical devices']
 
     df_arr = []
