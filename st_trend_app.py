2471bcdce04b37590df742e3a219081829a39ac4
rkreddyp <rkreddy@gmail.com>
2023-06-06 17:42:59 +0000

    fifth commit

--- st_trend_app.py
+++ st_trend_app.py
@@ -13,20 +13,20 @@ def get_data () :
 
 def draw_t_fig () :
     tdf = pd.read_csv ( "https://investrecipes.s3.amazonaws.com/apps/stockcharts_as/industries-rsi-adx-consolidated-stockcharts.csv")
-    print (tdf.shape)
-    kdf = pd.read_csv ('https://investrecipes.s3.amazonaws.com/koyfin_all_stocks.csv')
+    #print (tdf.shape)
+    #kdf = pd.read_csv ('https://investrecipes.s3.amazonaws.com/koyfin_all_stocks.csv')
 
-    kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (2Y FY)'] / kdf[ 'EV/Sales (EST FY1)' ]
+    #kdf['growth_evsales_ratio'] = kdf['Total Revenues/CAGR (2Y FY)'] / kdf[ 'EV/Sales (EST FY1)' ]
 
-    kdf['growth_evsales_ratio'] = pd.to_numeric (kdf['growth_evsales_ratio'])
+    #kdf['growth_evsales_ratio'] = pd.to_numeric (kdf['growth_evsales_ratio'])
 
-    kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] > 10 ]
+    #kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] > 10 ]
 
-    kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] < 100 ]
+    #kdf = kdf [ kdf [  'Net Income Margin % (FY)' ] < 100 ]
 
-    kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] > 10 ]
+    #kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] > 10 ]
 
-    kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] < 1000 ]
+    #kdf = kdf [ kdf [ 'Total Revenues/CAGR (2Y FY)' ] < 1000 ]
 
 
 
