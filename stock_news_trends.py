51cb305d7cc3c5cc647dc185af7794ecdb5cb00f
rkreddyp <rkreddy@gmail.com>
2023-06-08 01:47:52 +0000

    fifth commit

--- stock_news_trends.py
+++ stock_news_trends.py
@@ -103,7 +103,7 @@ def streamlit_main (url) :
 
             btdf.rename(columns={'clickable_url':'Source Link'}, inplace=True)
             st.write (btdf.columns.tolist())
-            btdf[['Stock','Source Link']].to_html('/tmp/btdf.html',escape=False, index=False)
+            btdf[['Stock','Reasons','Source Link']].to_html('/tmp/btdf.html',escape=False, index=False)
             
             with open('/tmp/btdf.html', 'r') as file:
                 html_string = file.read()
