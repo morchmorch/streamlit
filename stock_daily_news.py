3dbd6f8b28e4b6fbef7c76733a03fca397dfd570
Venkat <rkreddy@gmail.com>
2023-07-04 14:45:41 +0000

    fifth commit

--- stock_daily_news.py
+++ stock_daily_news.py
@@ -81,12 +81,13 @@ def streamlit_main (url) :
         with tab :
             df = pd.read_csv('https://investopsrecipes.s3.amazonaws.com/newsgpt/stock_recs.csv')
             st.write (df.shape)
-            cols = ['stock', 'stock_ticker', 'recommendation', 'sentiment', 'industry', 'reason', 'source_url', 'Source Link', 'news_summary']
+            #cols = ['stock', 'stock_ticker', 'recommendation', 'sentiment', 'industry', 'reason', 'source_url', 'Source Link', 'news_summary']
+            cols = ['stock', 'stock_ticker', 'recommendation', 'sentiment', 'industry', 'reason', 'Source Link']
 
             df [ 'clickable_url'  ] = df.apply(lambda row: "<a href='{}' target='_blank'>{}</a>".format(row['source_url'], "source link"), axis=1)
             df.rename(columns={'clickable_url':'Source Link'}, inplace=True)
             #st.write (btdf.columns.tolist())
-            df[cols].to_html('/tmp/df.html',escape=False, index=False)
+            df[cols].sort_values('sentiment', ascending=False).to_html('/tmp/df.html',escape=False, index=False)
             
             with open('/tmp/df.html', 'r') as file:
                 html_string = file.read()
