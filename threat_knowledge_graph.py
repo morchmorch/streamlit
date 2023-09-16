f5253a4c83c8ed962e387b1e61a9f554ce18d0c3
Ubuntu <rkreddy@gmail.com>
2023-09-16 22:30:39 +0000

    20_60

--- threat_knowledge_graph.py
+++ threat_knowledge_graph.py
@@ -17,5 +17,23 @@ st.set_page_config(page_title="Draft it for Me",layout='wide')
 
 url = 'https://investrecipes.s3.amazonaws.com/knowledge_graph.gv'
 r = requests.get(url, allow_redirects=True)
-
-st.graphviz_chart("knowledge_graph.gv")
+st.graphviz_chart('''
+    digraph {
+        run -> intr
+        intr -> runbl
+        runbl -> run
+        run -> kernel
+        kernel -> zombie
+        kernel -> sleep
+        kernel -> runmem
+        sleep -> swap
+        swap -> runswap
+        runswap -> new
+        runswap -> runmem
+        new -> runmem
+        sleep -> runmem
+    }
+''')
+with open("knowledge_graph.gv", 'r') as file:
+    data = file.read().replace('\n', '')
+st.graphviz_chart(data)
