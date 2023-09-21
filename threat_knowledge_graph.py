bfb4cee0e2522b13086db9d23436199cdf6128d3
Ubuntu <rkreddy@gmail.com>
2023-09-21 02:35:59 +0000

    added completion

--- threat_knowledge_graph.py
+++ threat_knowledge_graph.py
@@ -191,152 +191,158 @@ url = "https://thehackernews.com/2023/09/financially-motivated-unc3944-threat.ht
 url = "https://nvd.nist.gov/vuln/detail/CVE-2023-41331"
 
 title = st.text_input('Enter any URL or a CVE ID', 'CVE-2023-35708')
-if 'cve' in title.lower():
-    url = "https://nvd.nist.gov/vuln/detail/" + title
-else :
-    title = url
-#text = fetch_text(url)
-#st.write(text)
-#st.write(url)
-kc= fetch_text_requests(url)
-objective = "understand the attack details and remediations"
-kg_schema = openai_schema (KnowledgeGraph)
-system_content = "You are an an awesome information security engineer and detailed knowledge graph developer"
 
-prompt_content = """ 
- Your task is make the knowledge graph from an article for a given objective.
- objective : {objective}
- you must follow all reqirements that are listed below
- ## graph requirements
-  - in the edges list, count the number of target ids for each source ids, and the
-  number must be list_target_ids for each node
-  - when you make the edges, ensure that there must not be more than 10 target node 
-    ids connected to any given source node id in the graph. the number in list_target_ids
-    for any given source node id must therefore be at most 10
-  - for edge colors, you must make it visually informative.  you must use red color for 
-    important bad items , green color for good items, be creative for other colors
-  - there graph should be uni-directional, no bi-directional edges.  for example,
-    if node 1 is connected to node 2, then node 2 should not be connected to node 1
-  - the number of target nodes that are attached to source node of id 1 should be at most 5
-  - group all related targets belonging to a theme to one soruce id
-  - all nodes in the graph should be connected to atleast one other node
-  - all labels should be text and strings, not integers
-  - num_edges for all nodes should be 2 or more
-  - all nodes should be connected to node with source id 1 , directly or indirectly
-  - focus on specific information such time periods, dates, numbers , percentages, make them as edges such as "on" or "during"
-  - if time periods are mentioned, use them as edges and put the nodes and IDs in chronological order
-  - ensure to be as exhaustive as you can, include all the details from the article to acheive the objective
-  
-  # example of a good graph output
-   ```
-     {{'nodes': [{{'id': 1,
-    'label': 'Attack Details (SOFARPC)',
-    'color': 'blue',
-    'num_targets': 4,
-    'num_sources': 0,
-    'list_target_ids': [3,4,8,18],
-    'num_edges': 4}},
-    {{'id': 2,
-    'label': 'Java RPC framework',
-    'color': 'blue',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [1],
-    'num_edges': 2}},
-    {{'id': 3,
-    'label': 'Versions prior to 5.11.0',
-    'color': 'blue',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [1],
-    'num_edges': 2}},
-    {{'id': 4,
-    'label': 'Remote command execution',
-    'color': 'red',
-    'num_targets': 1,
-    'num_sources': 2,
-    'list_target_ids': [1,5],
-    'num_edges': 3}},
-    {{'id': 5,
-    'label': 'Payload',
-    'color': 'red',
-    'num_targets': 2,
-    'num_sources': 1,
-    'list_target_ids': [4,6,7],
-    'num_edges': 3}},
-    {{'id': 6,
-    'label': 'JNDI injection',
-    'color': 'red',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [5],
-    'num_edges': 2}},
-    {{'id': 7,
-    'label': 'System command execution',
-    'color': 'red',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [5],
-    'num_edges': 2}},
-    {{'id': 9,
-    'label': 'Version 5.11.0',
-    'color': 'green',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [8],
-    'num_edges': 2}},
-    {{'id': 18,
-    'label': 'Fix',
-    'color': 'green',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [1],
-    'num_edges': 2}},
-    {{'id': 8,
-    'label': 'Workarounds and Fixe Details',
-    'color': 'green',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [9],
-    'num_edges': 2}},
-    {{'id': 11,
-    'label': '-Drpc_serialize_blacklist_override=javax.sound.sampled.AudioFileFormat',
-    'color': 'green',
-    'num_targets': 1,
-    'num_sources': 1,
-    'list_target_ids': [18],
-    'num_edges': 2}},
-    ],
-  'edges': [{{'source': 1, 'target': 2, 'label': 'is a', 'color': 'blue'}},
-    {{'source': 1, 'target': 3, 'label': 'Versions prior to', 'color': 'blue'}},
-    {{'source': 1, 'target': 4, 'label': 'vulnerable to', 'color': 'red'}},
-    {{'source': 4, 'target': 5, 'label': 'can achieve', 'color': 'blue'}},
-    {{'source': 5, 'target': 6, 'label': 'or', 'color': 'red'}},
-    {{'source': 5, 'target': 7, 'label': 'or', 'color': 'red'}},
-    {{'source': 1, 'target': 8, 'label': 'work around', 'color': 'red'}},
-    {{'source': 8, 'target': 9, 'label': 'or', 'color': 'green'}},
-    {{'source': 1, 'target': 18, 'label': 'fixed by', 'color': 'red'}},
-    {{'source': 18, 'target': 11, 'label': 'or', 'color': 'green'}},
-    
-    ]}}
-   ```
-  
- think step by step, 
-  before you give the graph to the user =
-  - are the list_target_ids filled up for all nodes ?
-  - is the number of list_target_ids (num_targets) more than 8 ?
-  then re-do the graph
+sec_q_button=st.button(button_name, key = 'sec_q_button')
+st.markdown ( "--------")
+if sec_q_button or "enter" not in title.lower():
+    with st.spinner ( response_while ) :
 
- the attack article is as follows : 
-"""
+        if 'cve' in title.lower():
+            url = "https://nvd.nist.gov/vuln/detail/" + title
+        else :
+            title = url
+        #text = fetch_text(url)
+        #st.write(text)
+        #st.write(url)
+        kc= fetch_text_requests(url)
+        objective = "understand the attack details and remediations"
+        kg_schema = openai_schema (KnowledgeGraph)
+        system_content = "You are an an awesome information security engineer and detailed knowledge graph developer"
 
-prompt_string =prompt_content.format(objective = objective) + "\n" + str(kc)
-completion = chat_complete (model = "gpt-3.5-turbo-16k", system_content=system_content, temperature=0.5, user_content=prompt_string, functions = [kg_schema] ).completion
+        prompt_content = """ 
+        Your task is make the knowledge graph from an article for a given objective.
+        objective : {objective}
+        you must follow all reqirements that are listed below
+        ## graph requirements
+        - in the edges list, count the number of target ids for each source ids, and the
+        number must be list_target_ids for each node
+        - when you make the edges, ensure that there must not be more than 10 target node 
+            ids connected to any given source node id in the graph. the number in list_target_ids
+            for any given source node id must therefore be at most 10
+        - for edge colors, you must make it visually informative.  you must use red color for 
+            important bad items , green color for good items, be creative for other colors
+        - there graph should be uni-directional, no bi-directional edges.  for example,
+            if node 1 is connected to node 2, then node 2 should not be connected to node 1
+        - the number of target nodes that are attached to source node of id 1 should be at most 5
+        - group all related targets belonging to a theme to one soruce id
+        - all nodes in the graph should be connected to atleast one other node
+        - all labels should be text and strings, not integers
+        - num_edges for all nodes should be 2 or more
+        - all nodes should be connected to node with source id 1 , directly or indirectly
+        - focus on specific information such time periods, dates, numbers , percentages, make them as edges such as "on" or "during"
+        - if time periods are mentioned, use them as edges and put the nodes and IDs in chronological order
+        - ensure to be as exhaustive as you can, include all the details from the article to acheive the objective
+        
+        # example of a good graph output
+        ```
+            {{'nodes': [{{'id': 1,
+            'label': 'Attack Details (SOFARPC)',
+            'color': 'blue',
+            'num_targets': 4,
+            'num_sources': 0,
+            'list_target_ids': [3,4,8,18],
+            'num_edges': 4}},
+            {{'id': 2,
+            'label': 'Java RPC framework',
+            'color': 'blue',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [1],
+            'num_edges': 2}},
+            {{'id': 3,
+            'label': 'Versions prior to 5.11.0',
+            'color': 'blue',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [1],
+            'num_edges': 2}},
+            {{'id': 4,
+            'label': 'Remote command execution',
+            'color': 'red',
+            'num_targets': 1,
+            'num_sources': 2,
+            'list_target_ids': [1,5],
+            'num_edges': 3}},
+            {{'id': 5,
+            'label': 'Payload',
+            'color': 'red',
+            'num_targets': 2,
+            'num_sources': 1,
+            'list_target_ids': [4,6,7],
+            'num_edges': 3}},
+            {{'id': 6,
+            'label': 'JNDI injection',
+            'color': 'red',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [5],
+            'num_edges': 2}},
+            {{'id': 7,
+            'label': 'System command execution',
+            'color': 'red',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [5],
+            'num_edges': 2}},
+            {{'id': 9,
+            'label': 'Version 5.11.0',
+            'color': 'green',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [8],
+            'num_edges': 2}},
+            {{'id': 18,
+            'label': 'Fix',
+            'color': 'green',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [1],
+            'num_edges': 2}},
+            {{'id': 8,
+            'label': 'Workarounds and Fixe Details',
+            'color': 'green',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [9],
+            'num_edges': 2}},
+            {{'id': 11,
+            'label': '-Drpc_serialize_blacklist_override=javax.sound.sampled.AudioFileFormat',
+            'color': 'green',
+            'num_targets': 1,
+            'num_sources': 1,
+            'list_target_ids': [18],
+            'num_edges': 2}},
+            ],
+        'edges': [{{'source': 1, 'target': 2, 'label': 'is a', 'color': 'blue'}},
+            {{'source': 1, 'target': 3, 'label': 'Versions prior to', 'color': 'blue'}},
+            {{'source': 1, 'target': 4, 'label': 'vulnerable to', 'color': 'red'}},
+            {{'source': 4, 'target': 5, 'label': 'can achieve', 'color': 'blue'}},
+            {{'source': 5, 'target': 6, 'label': 'or', 'color': 'red'}},
+            {{'source': 5, 'target': 7, 'label': 'or', 'color': 'red'}},
+            {{'source': 1, 'target': 8, 'label': 'work around', 'color': 'red'}},
+            {{'source': 8, 'target': 9, 'label': 'or', 'color': 'green'}},
+            {{'source': 1, 'target': 18, 'label': 'fixed by', 'color': 'red'}},
+            {{'source': 18, 'target': 11, 'label': 'or', 'color': 'green'}},
+            
+            ]}}
+        ```
+        
+        think step by step, 
+        before you give the graph to the user =
+        - are the list_target_ids filled up for all nodes ?
+        - is the number of list_target_ids (num_targets) more than 8 ?
+        then re-do the graph
 
-#st.write (completion)
-visualize_knowledge_graph ( ast.literal_eval (completion['choices'][0].message['function_call']['arguments']) )
+        the attack article is as follows : 
+        """
 
-with open('/tmp/knowledge_graph.gv', 'r') as file:
-    file_contents = file.read()
+        prompt_string =prompt_content.format(objective = objective) + "\n" + str(kc)
+        completion = chat_complete (model = "gpt-3.5-turbo-16k", system_content=system_content, temperature=0.5, user_content=prompt_string, functions = [kg_schema] ).completion
 
-st.graphviz_chart (file_contents)
+        #st.write (completion)
+        visualize_knowledge_graph ( ast.literal_eval (completion['choices'][0].message['function_call']['arguments']) )
+
+        with open('/tmp/knowledge_graph.gv', 'r') as file:
+            file_contents = file.read()
+
+        st.graphviz_chart (file_contents)
